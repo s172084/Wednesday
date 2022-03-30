7c11086c2086bd3267cd8433916bc45cedea8e39
s172084 <s172084@dtu.dk>
2022-03-30 22:47:11 +0200

    changing my Time management file.

---
+++ Time_Management.R
@@ -0,0 +1,5 @@
+# This is my new file created directly from Github webpage online. 
+# In this file I would like to point out that the date is Wednesday 30 March 2022.
+# The time is 08:55 and I have been working since 06:55. 
+# In a few minutes it will be time for breakfast. 
+# Thankfully I have time to sit down and work on Github.
